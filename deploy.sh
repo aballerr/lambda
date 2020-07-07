338d2d93bd787d749a4343279d44753664c8a12b
Alexander Ball <aball9423@gmail.com>
2020-07-07 13:46:23 -0400

    testing deployment

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ echo "done installing, trying to deploy..."
 echo "zipping function.."
 zip function.zip handler.js
 echo "creating lambda.."
+aws lambda update-function-code --function-name my-function --zip-file fileb://function.zip
 # aws lambda create-function --function-name my-functionn \
 # --zip-file fileb://function.zip --handler index.handler --runtime nodejs12.x \
 # --role arn:aws:iam::867120318595:role/lambda-ex
