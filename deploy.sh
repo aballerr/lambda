2e5eb870d7e9f3b10db9114b4ba872e33f05c628
Alexander Ball <aball9423@gmail.com>
2020-07-06 23:48:46 -0400

    sanity test to see if pipeline is working

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+echo "installing node"
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt install -y nodejs
 node -v
 npm -v
-npm init -y
-npm i serverless
-npx serverless deploy
+echo "installing packages"
+npm install
+# npx serverless deploy
